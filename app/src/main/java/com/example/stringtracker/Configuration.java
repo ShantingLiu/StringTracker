249d9b2af28589732e282a3795e6f4ab6124ea40
swooo <liushanting@gmail.com>
2021-03-28 16:51:04 -0400

    Created skeleton structure of the activities

---
+++ app/src/main/java/com/example/stringtracker/Configuration.java
@@ -0,0 +1,14 @@
+package com.example.stringtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Configuration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+}
---
+++ app/src/main/java/com/example/stringtracker/Sentiments.java
@@ -0,0 +1,14 @@
+package com.example.stringtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sentiments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sentiments);
+    }
+}
