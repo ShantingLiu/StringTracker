229c199cc0aff7e8d627c3e6915a4a9fbed8bf61
swooo <liushanting@gmail.com>
2021-04-16 23:14:51 -0400

    Updated strings.xml to work with Ketih's code

---
+++ app/src/main/java/com/example/stringtracker/Configuration2.java
@@ -0,0 +1,13 @@
+package com.example.stringtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Configuration2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration2);
+    }
+}
