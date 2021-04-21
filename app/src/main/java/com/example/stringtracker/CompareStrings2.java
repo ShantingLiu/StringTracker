bbac0263adf0a7d98a1ec58fa82b6cddeefccd67
Jairopian <jairopcruz@gmail.com>
2021-04-20 23:35:58 -0400

    added return from comparestring

--- app/src/main/java/com/example/stringtracker/CompareStrings2.java
+++ app/src/main/java/com/example/stringtracker/CompareStrings2.java
@@ -2,7 +2,10 @@ package com.example.stringtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -14,12 +17,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CompareStrings2 extends AppCompatActivity {
+    // local copies of the main stringset, instrument, and appstate objects
+    AppState A1 = new AppState();
+    StringSet S1 = new StringSet();
+    StringSet S2 = new StringSet();   // 2nd stringset for comparison
+    Instrument I1 = new Instrument();
 
     LineChart Linegraph;
+    Button buttonRet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_strings2);
+
+        //dbstuff i believe
+        String appState;
+        String instState;
+        String strState;
+        Intent mIntent = getIntent();      // passing stringset object states into compare activity
+        appState = mIntent.getStringExtra("appstate");
+        instState = mIntent.getStringExtra("inststate");
+        strState = mIntent.getStringExtra("strstate");
+        A1.setAppState(appState);
+        I1.setInstState(instState);
+        S1.setStrState(strState);
+
    // in this example, a LineChart is initialized from xml
         Linegraph = (LineChart) findViewById(R.id.linegraph);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -46,5 +69,22 @@ public class CompareStrings2 extends AppCompatActivity {
         Linegraph.setData(data);
 
 
-    }
-    }
+        // took return button method to go back to screen with out it can leave page
+        buttonRet = findViewById(R.id.returnline);
+        buttonRet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                //A1.setInstrumentID(888);
+                //A1.saveRunState();  // DEBUG using stored data file for messaging
+                resultIntent.putExtra("appstate", A1.getAppState());
+                resultIntent.putExtra("inststate", I1.getInstState());
+                resultIntent.putExtra("strstate", S1.getStrState());
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+
+
+    }//oncreate
+}
